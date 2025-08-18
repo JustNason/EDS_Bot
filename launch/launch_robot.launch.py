a9cba50ebbc6b7c65f6d8ecb6908d43f0cb5eb2d
Nason McGillin <nason321@yahoo.com>
2025-08-17 20:56:00 -0700

    im looking into it: hopefully increased some delays on controller spawning

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -1,83 +1,67 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, TimerAction
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command
 from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessStart
-
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-
-
 
 def generate_launch_description():
 
+    package_name = 'eds_bot'
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='eds_bot' #<--- CHANGE ME
-
+    # --- robot_state_publisher ---
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name),'launch','rsp.launch.py'
+        )]),
+        launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
 
-    # joystick = IncludeLaunchDescription(
-    #             PythonLaunchDescriptionSource([os.path.join(
-    #                 get_package_share_directory(package_name),'launch','joystick.launch.py'
-    #             )])
-    # )
-
-
+    # --- twist_mux ---
     twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
     twist_mux = Node(
-            package="twist_mux",
-            executable="twist_mux",
-            parameters=[twist_mux_params],
-            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
-        )
+        package="twist_mux",
+        executable="twist_mux",
+        parameters=[twist_mux_params],
+        remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+    )
 
-    
+    # --- robot_description directly from URDF ---
+    urdf_file = os.path.join(get_package_share_directory(package_name), 'urdf', 'robot.urdf')
+    with open(urdf_file, 'r') as infp:
+        robot_description = {'robot_description': infp.read()}
 
-
-    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
-
+    # --- controller manager ---
     controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
-
     controller_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[{'robot_description': robot_description},
-                    controller_params_file]
+        parameters=[robot_description, controller_params_file]
     )
+    delayed_controller_manager = TimerAction(period=5.0, actions=[controller_manager])
 
-    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
-
+    # --- spawners ---
     diff_drive_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["diff_cont"],
     )
-
-    delayed_diff_drive_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(
-            target_action=controller_manager,
-            on_start=[diff_drive_spawner],
-        )
-    )
-
     joint_broad_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["joint_broad"],
     )
 
+    # --- Ensure spawners start only after controller_manager ---
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[diff_drive_spawner],
+        )
+    )
     delayed_joint_broad_spawner = RegisterEventHandler(
         event_handler=OnProcessStart(
             target_action=controller_manager,
@@ -85,20 +69,20 @@ def generate_launch_description():
         )
     )
 
-    lidar = IncludeLaunchDescription(
+    # --- LiDAR ---
+    lidar_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory(package_name),'launch','ld19lidar.launch.py'
         )])
     )
-
     delayed_lidar = RegisterEventHandler(
         event_handler=OnProcessStart(
-            target_action=diff_drive_spawner,  # wait for diff_drive_spawner to start
-            on_start=[lidar]
+            target_action=diff_drive_spawner,
+            on_start=[lidar_launch]
         )
     )
 
-    # Launch them all!
+    # --- Return LaunchDescription ---
     return LaunchDescription([
         rsp,
         twist_mux,
